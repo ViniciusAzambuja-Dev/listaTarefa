3fac2ae94c4c83fa259444c2be7e6289c4f09141
Vinicius Azambuja <vinizambs@gmail.com>
2024-03-18 01:27:25 -0300

    Adicionar função de deletar lista

--- js/teste.js
+++ js/teste.js
@@ -1,45 +1,51 @@
 let input = document.getElementById("digiteTarefa");
-let buttonAdd = document.getElementById("buttonADD");
 let main = document.getElementById("areaLista");
 let contador = 0;
 
 function adicionar(){
-        let novoItem;
-        valorInput = input.value;
+    let novoItem;
+    valorInput = input.value;
 
-        if((valorInput !== null) && (valorInput !== "")){
-            contador++
+    if((valorInput !== null) && (valorInput !== "")){
+        contador++
 
-            novoItem = `<div class="item" id= ${contador} >
-            <div onclick="check(${contador})" class="icone_Div">
-                <i id=icone_${contador} class="mdi mdi-circle-outline"></i>
-            </div>
+        novoItem = `<div class="item" id= ${contador}>
+        <div onclick="check(${contador})" class="icone_Div">
+            <i id=icone_${contador} class="mdi mdi-circle-outline"></i>
+        </div>
             
-            <div class="nome_Tarefa"> ${valorInput}</div>
-            <div class="div_Button_Deletar">
-                <button type="button" class="button_Deletar">Deletar</button>
-            </div>
-            </div>`
+        <div class="nome_Tarefa"> ${valorInput}</div>
+        <div onclick="deletar(${contador})" class="div_Button_Deletar">
+            <button type="button" class="button_Deletar">Deletar</button>
+        </div>
+
+        </div>`
             
             main.innerHTML += novoItem;
-            
+            input.value = "";
         }
-
-
 }
 
 function check(id){
-      let item = document.getElementById(id);
-      let classe = item.getAttribute('class');
+      var item = document.getElementById(id);
+      var classe = item.getAttribute('class');
      
 
       if(classe == "item"){
         item.classList.add('itemclicado');
-        let icone = document.getElementById("icone_"+ id);
+
+        var icone = document.getElementById("icone_"+ id);
         icone.classList.remove('mdi-circle-outline');
         icone.classList.add('mdi-check-circle');
       }
-
-      
-    
 }
+
+function deletar(id){
+    var itemRemover = document.getElementById(id);
+    itemRemover.remove();
+}
+
+function focar(){
+    input.focus();
+}
+
